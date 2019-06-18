b0366c96a1caf5ea9fc598fa7807ff35ba5bde33
nakashima <nakashima@biocomputing.cc>
2019-06-18 20:09:27 +0900

    chapter2

---
+++ nakashima/chapter2/2-2.rb
@@ -0,0 +1,6 @@
+puts "Ruby"
+
+puts "abc" + "def"
+
+puts "123".to_s + "456".to_s
+
---
+++ nakashima/chapter2/2-3.rb
@@ -0,0 +1,10 @@
+coffee = 300
+esplesso = 100
+
+puts "コーヒ: #{coffee}"
+puts "合計: #{coffee + esplesso * 2}"
+
+coffee = 400
+
+puts "コーヒ: #{coffee}"
+puts "合計: #{coffee + esplesso * 2}"
