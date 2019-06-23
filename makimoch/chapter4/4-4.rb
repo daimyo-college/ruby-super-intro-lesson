4210768ec14cffd8845a97081b2b7fc0e6ec8698
makimoch <1610.baumkuchen.124@gmail.com>
2019-06-23 15:01:37 +0900

    chapter4 4-1, 4-3, 4-4 を提出します

---
+++ makimoch/chapter4/4-4.rb
@@ -0,0 +1,25 @@
+# Q8
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts drink
+end
+
+# Q9
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts drink + "お願いします"
+end
+
+# Q10
+sum = 0
+numbers = [1, 2, 3]
+numbers.each do |number|
+  sum += number
+end
+puts sum
+
+# Q11
+drinks = []
+drinks.each do |drink|
+  puts drink
+end
