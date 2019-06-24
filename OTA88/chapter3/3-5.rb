9c96b6e4df01f7b40b963509995ad51ca309eb8b
akiota <aki.ota@gmail.com>
2019-06-24 20:10:37 +0900

    “3-4,3-5,3-6,3つのfileを追加”

---
+++ OTA88/chapter3/3-5.rb
@@ -0,0 +1,15 @@
+#ゼロからわかるRuby超入門 -3章練習問題-
+
+#3-5
+# season = gets #春を入力=>"あんまんを買ってこう！"が返る。なぜ？
+# puts season
+# puts season.class
+season = "春"
+case season
+when season = "春"
+  puts "アイスを買っていこう！"
+when season = "夏"
+  puts "かき氷を買ってこう！"
+else
+  puts "あんまんを買ってこう！"
+end
