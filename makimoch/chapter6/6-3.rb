d0c22320852a558f3c14a4793687b406f5583c31
makimoch <1610.baumkuchen.124@gmail.com>
2019-06-25 12:55:30 +0900

    6章を追加

---
+++ makimoch/chapter6/6-3.rb
@@ -0,0 +1,28 @@
+# Q8
+menu = {"コーヒー" => 300, "カフェラテ" => 400}
+menu.each{|key, value|
+  puts "#{key} - #{value}円"
+}
+# keyも式展開の形で一緒に書いてしまうほうがいいのかも？
+
+# Q9
+menu = {"コーヒー" => 300, "カフェラテ" => 400}
+menu.each do |key, value|
+  if value>= 350
+    puts "#{key} - #{value}円"
+  end
+end
+
+# Q10
+menu = {}
+menu.each do |key, value|
+  puts "#{key} - #{value}円"
+end
+
+# Q11
+menu = {"コーヒー" => 300, "カフェラテ" => 400}
+array = []
+menu.each do |key, value|
+  array.push(key)
+end
+p array
