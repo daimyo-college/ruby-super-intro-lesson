a90f389ef127b48efb1e2c47919de4217ed308c6
nakashima <nakashima@biocomputing.cc>
2019-06-28 14:51:58 +0900

    homework-ch9

---
+++ nakashima/chapter9/2.rb
@@ -0,0 +1,20 @@
+module ChocolateChip
+  def chocolate_chip
+    @name += "チョコレートチップ"
+  end
+end
+
+class Drink
+  include ChocolateChip
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
+
+drink = Drink.new("モカ")
+drink.chocolate_chip
+
+puts drink.name
