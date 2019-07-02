914c2641bab26962b01c8f08fc2b554b24da11a1
alt9800 <sd193005@cis.fukuoka-u.ac.jp>
2019-07-02 13:12:35 +0900

    8章の提出です...tanaka

---
+++ tanaka/chapter8/8-2.rb
@@ -0,0 +1,4 @@
+class CaffeLatte
+end
+caffe_latte = CaffeLatte.new
+p caffe_latte.class
---
+++ tanaka/chapter8/8-6.rb
@@ -0,0 +1,6 @@
+class Drink
+    def self.todays_special
+         "ホワイトモカ"
+    end
+end
+puts Drink.todays_special
