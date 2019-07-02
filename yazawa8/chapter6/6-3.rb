aae446c5adeef5a0b0b80c81e872b8c72b24866a
yazawa <yaza_ta@yahoo.co.jp>
2019-07-02 18:09:07 +0900

    yazawa 課題提出 chapter6 修正分

--- yazawa8/chapter6/6-3.rb
+++ yazawa8/chapter6/6-3.rb
@@ -1,18 +1,23 @@
+#問8
 menu = {"コーヒー" => 300, "カフェラテ" => 400}
 menu.each do |key, value|
   p "#{key} - #{value}円"
 end
 
+#問9
 menu.each do |key, value|
-  if value > 350
+  if value >= 350
     p "#{key} - #{value}円"
   end
 end
 
+#問10
 menu = {}
 menu.each do |key, value|
   p "#{key} - #{value}円"
 end
 
+#問11
 menu = {"コーヒー" => 300,"カフェラテ" => 400}
 p menu.keys
+
