082afe1b81770137f4814dc46e5d351a083caeda
YONO <y.nozz0119@gmail.com>
2019-07-06 14:13:55 +0900

    Chapter4の提出

---
+++ ono/chapter4/4-4.rb
@@ -0,0 +1,26 @@
+#8
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+  drinks.each do |drink|
+    puts drink
+  end
+
+#9
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+  drinks.each do |drink|
+    puts drink + "でお願いします"
+  end
+
+#10
+sum = 0
+a = [1, 2, 3]
+  a.each do |x|
+    sum = sum + x
+  end
+  puts sum
+
+
+#11
+drinks = []
+  drinks.each do |drink|
+    puts drink
+  end
