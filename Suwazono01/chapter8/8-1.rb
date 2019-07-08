0c349b4ceb12cc6855cefb4ce4495249686ef3ce
suwazono <pancake0147@icloud.com>
2019-07-09 06:34:33 +0900

    chapter8

---
+++ Suwazono01/chapter8/8-1.rb
@@ -0,0 +1,6 @@
+p "問１"
+menu = {:coffee => 300, :caffe_latte => 400}
+p menu.class
+
+p "問２"
+Hash.new
---
+++ Suwazono01/chapter8/8-2.rb
@@ -0,0 +1,6 @@
+p "問３"
+class CaffeLatte
+end
+
+caffelatte=CaffeLatte.new
+p caffelatte.class
