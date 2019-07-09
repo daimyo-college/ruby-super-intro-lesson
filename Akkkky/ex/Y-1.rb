127f44ddec0cffa8dd4e145ac93667f8f44433f2
Akkkky <coding_addiction@yahoo.co.jp>
2019-07-09 18:39:55 +0900

    問Y-1修正します

--- Akkkky/ex/Y-1.rb
+++ Akkkky/ex/Y-1.rb
@@ -1,10 +1,10 @@
-str_ancestors =  String.ancestors
-ary_ancestors = Array.ancestors
+str_ancestors =  "aaa".class.ancestors
+ary_ancestors = [1, 2, 3].class.ancestors
 
 class MyClass
 end
 
-my_class_ancestors = MyClass.ancestors
+my_class_ancestors = MyClass.class.ancestors
 
 p "Stringクラスの親クラス：#{str_ancestors}"
 p "Arrayクラスの親クラス：#{ary_ancestors}"
