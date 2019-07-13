4ad1a63458b6e8e09f64104a024240e3c48f9afe
木下雄貴 <kinos@MacBook-Air.local>
2019-07-13 23:24:03 +0900

    課題提出　木下雄貴　chapter3

---
+++ kinos/chapter3/3-2.rb
@@ -0,0 +1,14 @@
+# 問３
+puts "問３"
+season = "春"
+if season != "夏"
+  puts "あんまんが食べたい"
+end
+
+# 問4
+puts "問4"
+season = "夏"
+if season = "夏"
+  puts "カキ氷が食べたい"
+  puts "麦茶飲みたい"
+end
---
+++ kinos/chapter3/3-6.rb
@@ -0,0 +1,9 @@
+# 問９
+puts "問９"
+menus = ["カフェラテ","モカ","モカ"]
+2.times do
+  menus.each do |menu|
+    puts menu
+  end
+end
+puts "フラペチーノ"
