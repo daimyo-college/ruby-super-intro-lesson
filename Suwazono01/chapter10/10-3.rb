74ec3df3c78fd80077dd45d68ed70fad26be4c7b
suwazono <pancake0147@icloud.com>
2019-07-16 13:29:57 +0900

    chapter10

---
+++ Suwazono01/chapter10/10-3.rb
@@ -0,0 +1,9 @@
+p "問２"
+require "net/http"
+uri = URI.parse("https://localhost:4567/hi")
+puts Net::HTTP.get(uri)
+
+# p "問３"
+# require "net/http"
+# uri = URI.parse("https://localhost:4568/hi")
+# puts Net::HTTP.get(uri)
