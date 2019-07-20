82f99caeeee6d92b77ea3f0835ba6e4c430ef73f
suwazono <pancake0147@icloud.com>
2019-07-20 11:28:38 +0900

    chapter8

---
+++ Suwazono01/chapter8/8-4.rb
@@ -0,0 +1,13 @@
+p "問５"
+class Item
+  def name=(text)
+    @name = text
+  end
+  def name
+    @name
+  end
+end
+
+item = Item.new
+item.name= "チーズケーキ"
+puts item.name
---
+++ Suwazono01/chapter8/8-5.rb
@@ -0,0 +1,12 @@
+p "問６"
+class Item
+  def initialize
+    puts "商品を扱うオブジェクト"
+  end
+end
+
+Item.new
+
+p "問７"
+class Item
+  
