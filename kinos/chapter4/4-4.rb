7a89edfa2f8ac685861e7d4df792fd4e2c706556
木下雄貴 <kinos@MacBook-Air.local>
2019-07-20 11:43:57 +0900

    課題提出　木下雄貴　chapter4

---
+++ kinos/chapter4/4-4.rb
@@ -0,0 +1,32 @@
+#問８
+puts "問８"
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts drink
+end
+
+
+#問９
+puts "問９"
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts drink + "お願いします"
+end
+
+
+#問１０
+puts "問１０"
+numbers = [1, 2, 3]
+numbers.each do |number|
+ sum += number
+end
+puts sum
+
+
+#問１１
+puts "問１１"
+drinks = []
+drinks.each do |drink|
+  puts drink
+end
+
